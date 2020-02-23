992cb087fa10c248da2c92a4e78172c2d81da75a
emailkhatri2020 <emailkhatri2020@gmail.com>
2020-02-23 17:31:23 -0500

    movie modle has been added

--- gum/models.py
+++ gum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class movies(models.Model):
+    movie_name = models.CharField(max_length = 100)
+    movie_released = models.DateField()
+    movie_generay = models.CharField(max_length = 25)
+    movie_type = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.movie_name
