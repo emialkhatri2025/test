0074dfecfa6b804367a319dc3e093904cb85d89a
nepal2020 <nepal.nepal58@yahoo.com>
2020-04-10 20:14:36 -0400

    few lines has been added to the test.py file

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-hello
+def feb():
+    n1 = 0
+    n2 = 1
+    count = 10
+    for i in range(2, count):
+        i = n1 + n2
+        n1 = n2
+        n2 = i
+        print("The feb series is :",i)
+feb()
+
+
+    
+
 
 
